bdf42432e532732a85d14b69f7d962d81e79fb9b
Christerpher Hunter <djhunter67@gmail.com>
2025-04-19 08:23:48 -0400

    fix: return proper error from server

--- src/endpoints/scan.rs
+++ src/endpoints/scan.rs
@@ -1,9 +1,13 @@
-use actix_web::{get, http::header::ContentType, web, HttpResponse};
+use actix_web::{
+    get,
+    http::{header::ContentType, StatusCode},
+    web, HttpResponse,
+};
 use askama::Template;
-use mdns_scanner::{mdns_scan, Service, ServiceDetect};
+use mdns_scanner::{mdns_scan, ServiceDetect};
 use tracing::{error, info, instrument};
 
-use crate::endpoints::templates::ScanResult;
+use crate::endpoints::templates::{ErrorPage, ScanResult};
 
 #[instrument(
     name = "Serving main page",
@@ -25,18 +29,37 @@ pub async fn scan_tech(tech_to_scan: web::Path<String>) -> HttpResponse {
         Ok(res) => res,
         Err(err) => {
             error!("Scan result error or no results: {err}");
-            vec![Service::default()]
+            vec![]
         }
     };
 
+    if scan.is_empty() {
+        let error_template = ErrorPage {
+            title: "Scan Error",
+            code: u32::from(StatusCode::INTERNAL_SERVER_ERROR.as_u16()),
+            error: "Scan Error",
+            message: "No results found for the specified technology.",
+        };
+        return HttpResponse::InternalServerError()
+            .content_type(ContentType::html())
+            .body(
+                error_template
+                    .render()
+                    .expect("Failed to render ERROR template"),
+            );
+    }
     let var_name = ScanResult {
         scan_domain: &tech_to_scan.into_inner(),
         results: scan,
     };
-
     let rendered = var_name.render().expect("Failed to render template");
-
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(rendered)
+
+    // let rendered = var_name.render().expect("Failed to render template");
+
+    // HttpResponse::Ok()
+    //     .content_type(ContentType::html())
+    //     .body(rendered)
 }
